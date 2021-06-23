36bd21857fd30351f6fff4864f91d2ee67916074
Ayushi41 <52519951+Ayushi41@users.noreply.github.com>
2021-06-24 01:44:10 +0530

    Add files via upload
    change commit

---
+++ precache-manifest.4109848df11250dbd5269dca72247676.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f0ac1cb56e6b6dad8b035b0dc04e95",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "113d6a0bb9629dff5749",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "be384bf701e7e1a9e069",
+    "url": "/home/static/js/2.ef3efb9f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.ef3efb9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "113d6a0bb9629dff5749",
+    "url": "/home/static/js/main.0c7d0a00.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "bfb111a7b28b9fdc139fc15da2e8cdef",
+    "url": "/home/static/media/cogni.bfb111a7.jpg"
+  },
+  {
+    "revision": "267e763a2ae6ca134e94b2de13e2b754",
+    "url": "/home/static/media/football.267e763a.jpg"
+  },
+  {
+    "revision": "61bb28a27c2206544069675fd30c310c",
+    "url": "/home/static/media/music.61bb28a2.jpg"
+  },
+  {
+    "revision": "217bfd91314781ac9bc813e6ae4d4945",
+    "url": "/home/static/media/resume.217bfd91.pdf"
+  },
+  {
+    "revision": "d7dc66428cafccf16d20532a14685fba",
+    "url": "/home/static/media/swapnilsen.d7dc6642.jpeg"
+  }
+]);
